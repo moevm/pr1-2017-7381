c49087b622341b44bfc0aa23c320f567cd168bfd
Makkksx <m.vologdin@mail.ru>
2017-11-02 19:06:08 +0300

    Vologdin_lr2_done

---
+++ Vologdin_MD_lr2/index_last_negative.h
@@ -0,0 +1,4 @@
+#ifndef  __INDEX_LAST_NEGATIVE_H__
+#define __INDEX_LAST_NEGATIVE_H__
+int index_last_negative(int mas[], int ssize);
+#endif
---
+++ Vologdin_MD_lr2/sum_before_and_after_negative.h
@@ -0,0 +1,5 @@
+#ifndef  __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
+#define __SUM_BEFORE_AND_AFTER_NEGATIVE_H__
+int sum_before_and_after_negative(int mas[], int ssize);
+#endif
+
