8d472fb30dbd44b8beb6e1f817b0809136738cd4
Joz <marklauc1998@gmail.com>
2017-11-24 00:53:53 +0300

    Laucius LR №2 done

---
+++ Laucius_Mark_lr2/sum_between.c
@@ -0,0 +1,12 @@
+#include "index_first_zero.h"
+#include "index_last_zero.h"
+
+int sum_between(int mass[], int size) {
+    int first = index_first_zero(mass, size),
+        last = index_last_zero(mass, size),
+        result = 0,
+        i = first;
+    for (; i < last; i++)
+        result += (mass[i]>0 ? mass[i] : -mass[i]);
+    return result;
+}
