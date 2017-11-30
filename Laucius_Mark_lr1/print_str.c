364aa81deec96a0675ae7f51379c12a88b8427c8
MarkLaucius <31574856+MarkLaucius@users.noreply.github.com>
2017-11-30 18:11:30 +0300

    Update print_str.c

--- Laucius_Mark_lr1/print_str.c
+++ Laucius_Mark_lr1/print_str.c
@@ -1,3 +1,4 @@
-#include<stdio.h>                                                               
+#include<stdio.h>        
+#include "print_str.h"
 void print_str(const char *str){                                                
 puts(str);} 
