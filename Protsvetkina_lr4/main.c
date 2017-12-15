9e2e8ff37388003180f9d91641ea540f75481ac0
Anastasiya <asifyoucared@mail.ru>
2017-12-15 13:13:21 +0300

    lols

--- Protsvetkina_lr4/main.c
+++ Protsvetkina_lr4/main.c
@@ -46,8 +46,8 @@ int main(){
         names[i] = (char*)malloc(sizeof(char*) * (strlen(name)+1));
         authors[i] = (char*)malloc(sizeof(char*) * (strlen(author)+1));
 
-        strcpy(names[i], name);
-        strcpy(authors[i], author);
+        strncpy(names[i], name, 80);
+        strncpy(authors[i], author, 80);
 
     }
     MusicalComposition* head = createMusicalCompositionList(names, authors, years, length);
