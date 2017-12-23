83729fa046f9305b7a8fa83618f6f1aa49e534b5
YMashina <31595546+YMashina@users.noreply.github.com>
2017-12-23 22:41:13 +0300

    Update main.c

--- Mashina_Yulia_lr4/main.c
+++ Mashina_Yulia_lr4/main.c
@@ -4,24 +4,28 @@
 
 typedef struct MusicalComposition 
 {
-     char name[80];
-     char author[80];
+     char* name;
+     char* author;
      int year;
      struct MusicalComposition* next;
      struct MusicalComposition* prev;
 }MusicalComposition;
+
 MusicalComposition* createMusicalComposition(char* name, char* author, int year)
 {
     MusicalComposition* composition = (MusicalComposition*) malloc(sizeof(MusicalComposition));
-    strcpy(composition->name, name);
-    strcpy(composition->author, author);
+    composition->name = name;
+    composition->author = author;
     composition->year = year;
     composition->next = NULL;
     composition->prev = NULL;
     return composition;
 }
+
 MusicalComposition* createMusicalCompositionList(char** array_names, char** array_authors, int* array_years, int n)
 {
+    if(n==0) 
+        return NULL;
     int i = 1;
     MusicalComposition *head = createMusicalComposition(array_names[0], array_authors[0], array_years[0]);
     MusicalComposition *list = head;
@@ -32,13 +36,69 @@ MusicalComposition* createMusicalCompositionList(char** array_names, char** arra
     }
     return head;
 }
-void push(MusicalComposition* head, MusicalComposition* element);
-
-void removeEl(MusicalComposition* head, char* name_for_remove);
-
-int count(MusicalComposition* head);
-
-void print_names(MusicalComposition* head);
+void push(MusicalComposition** phead, MusicalComposition* element)
+{
+    MusicalComposition* head = *phead;  
+    if(!head) {
+        *phead=element; return;
+    }
+    while (head->next != NULL)
+    {
+        head = head->next;
+    }
+    head->next = element;
+    element->prev = head;
+}
+void removeEl(MusicalComposition** phead, char* name_for_remove)
+{
+    MusicalComposition* head = *phead;
+    for(;head!=NULL;)
+    {
+        
+        if(strcmp(head->name,name_for_remove))
+        {
+            head = head->next;
+        }
+ 
+      else {  
+         if (head->prev==NULL) 
+        {
+            if (head->next==NULL)
+            {
+                *phead=head->next;
+                free(head); break;
+            }
+            *phead=head->next;
+            head=head->next;
+            free(head->prev);
+            head->prev=NULL;
+            return;
+        }
+        if (head->next==NULL)
+        {
+            head->prev->next = head->next;
+            free(head); break;
+        }
+        head->prev->next = head->next;
+        head->next->prev = head->prev;
+        MusicalComposition* list = head->next;
+        free(head);
+        head=list;
+        }
+   }
+}
+int count(MusicalComposition* head)
+{
+    int amm = 0;
+    for (; head!=NULL; head=head->next)
+        amm++;
+    return amm;
+}
+void print_names(MusicalComposition* head)
+{
+    for (; head != NULL; head=head->next)
+        printf("%s\n", head->name);
+}
 
 int main(){
     int length;
@@ -85,16 +145,16 @@ int main(){
     fgets(name_for_remove, 80, stdin);
     (*strstr(name_for_remove,"\n"))=0;
 
-    printf("%s %s %d\n", head->name, head->author, head->year);
+    if (head) printf("%s %s %d\n", head->name, head->author, head->year);
     int k = count(head);
 
     printf("%d\n", k);
-    push(head, element_for_push);
+    push(&head, element_for_push);
 
     k = count(head);
     printf("%d\n", k);
 
-    removeEl(head, name_for_remove); 
+    removeEl(&head, name_for_remove); 
     print_names(head);
 
     k = count(head);
@@ -105,40 +165,5 @@ int main(){
         free(authors[i]);
     }
     free(names);
-    free(authors);
-    free(years);
-
-    return 0;
-
-}
-void push(MusicalComposition* head, MusicalComposition* element)
-{
-    MusicalComposition* list = head;
-    while (list->next != NULL)
-    {
-        list = list->next;
-    }
-    list->next = element;
-    element->prev = list;
-}
-void removeEl(MusicalComposition* head, char* name_for_remove)
-{
-    for(;strcmp(head->name,name_for_remove);)
-    {
-        head = head->next;
-    }
-    head->prev->next = head->next;
-    head->next->prev = head->prev;
-}
-int count(MusicalComposition* head)
-{
-    int amm = 0;
-    for (MusicalComposition* list=head; list!=NULL; list=list ->next)
-        amm++;
-    return amm;
-}
-void print_names(MusicalComposition* head)
-{
-    for (MusicalComposition* list = head; list != NULL; list=list->next)
-        printf("%s\n", list->name);
+return 0; 
 }
